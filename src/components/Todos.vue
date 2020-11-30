979495121deaf0f28c00eb417c990a099a82ad4d
AnstettVictor <anstett.victor67@gmail.com>
2020-11-30 14:28:36 +0100

    ajout methode computed all done

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -6,15 +6,25 @@
             <input type="text" class="new-todo" placeholder="Ajouter une tache" v-model="newTodo" @keyup.enter="addTodo">
         </header>
         <div class="main">
+            <input type="checkbox" class="toggle-all" v-model="allDone">
             <ul class="todo-list">
-                <li class="todo" v-for="todo in todos" :class="{completed: todo.completed }">
+                <li class="todo" v-for="todo in filteredTodos" :class="{completed:todo.completed}">
                     <div class="view">
-                        <input  class="togle" type="checkbox" v-model="todo.completed">
+                        <input type="checkbox" v-model="todo.completed" class="toggle" >
                         <label>{{todo.name}}</label>
                     </div>
                 </li>
             </ul>
         </div>
+        <footer class="footer">
+            <span class="todo-count"><strong>{{ remaining }}</strong> Tâches à faires</span>
+            <ul class="filters">
+                
+                <li><a href="#" :class="{selected: filter === 'all'}" @click.prevent="filter = 'all'" >Toutes</a></li>
+                <li><a href="#" :class="{selected: filter === 'todo'}" @click.prevent="filter ='todo'">A faire</a></li>
+                <li><a href="#" :class="{selected: filter === 'done'}"  @click.prevent="filter = 'done'">Faites</a></li>
+            </ul>
+        </footer>
     </section>
 </template>
 
@@ -23,11 +33,13 @@
 export default {
     data () {
         return {
+            
             todos: [{
                 name: 'Tache de test',
                 completed: false,
             }],
-            newTodo: ''
+            newTodo: '',
+            filter: 'all'
         }
     },
     methods: {
@@ -37,6 +49,29 @@ export default {
                 name: this.newTodo
             })
             this.newTodo= ''
+        }
+    },
+    computed: {
+         allDone: {
+        get () {
+          return this.remaining === 0
+        },
+        set (value) {
+          this.todos.forEach(todo => {
+            todo.completed = value
+          })
+        }
+      },
+        remaining () {
+            return this.todos.filter(todo => !todo.completed).length
+        },
+        filteredTodos () {
+            if(this.filter === 'todo') {
+                return this.todos.filter(todo => !todo.completed)
+            } else if (this.filter === 'done') {
+                return this.todos.filter(todo => todo.completed)
+            }
+            return this.todos
         }
     }
 }
